fea90296ee5e7a12c592688e3769e30cd49cdd36
rafaelpadilha <rafaelpadilha@hotmail.com.br>
2020-03-09 00:29:36 -0300

    Adding class configurations.

--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
 import logging
 
+
 def app_cfg():
     config = {}
-    config['path']= "/home/rafael/ifb/tcc/cnn-classification-fabric/"
-    config['data_path']= config['path'] + "data/simpsons.pickle"
-    config['img_size']=64
-    config['class_numb']=24
+    config['path'] = "/home/rafael/ifb/tcc/cnn-classification-fabric/"
+    config['data_path'] = config['path'] + "data/fabric.pickle"
+    config['img_size'] = 150
+    config['class_names'] = ['tela', 'sarja', 'cetim']
+    config['class_n'] = len(config['class_names'])
     return config
+
 
 logging.basicConfig(
     format=("%(asctime)s,%(msecs)-3d - %(name)-12s - %(levelname)-8s => %(message)s"),
