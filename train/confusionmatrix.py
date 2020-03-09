48c75f1121ff6139e0367fd66c0d46281ebe57bd
rafaelpadilha <rafaelpadilha@hotmail.com.br>
2020-03-09 00:26:54 -0300

    Adding confusion matrix methods.

---
+++ train/confusionmatrix.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from itertools import product
+import numpy as np
+from io import BytesIO
+import tensorflow as tf
+from sklearn import metrics
+
+class ConfusionMatrix():
+    def __init__(self, logdir, filename='confusion_matrix.jpg'):
+        self.filename=filename
+        self.logdir=logdir
+
+    def save_cm(self, cm,class_names):
+        plt.figure(figsize=(8, 8))
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title("Confusion matrix")
+        plt.colorbar()
+        tick_marks = np.arange(len(class_names))
+        plt.xticks(tick_marks, class_names, rotation=45)
+        plt.yticks(tick_marks, class_names)
+
+        # Normalize the confusion matrix.
+        cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+        # Use white text if squares are dark; otherwise black.
+        threshold = cm.max() / 2.
+        for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+            color = "white" if cm[i, j] > threshold else "black"
+            plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.savefig(self.logdir + self.filename)
