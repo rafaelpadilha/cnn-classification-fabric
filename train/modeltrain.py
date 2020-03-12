bfd66975b597a40db0e7153b491cc697093300bf
Rafael Padilha <rafaelpadilha@hotmail.com.br>
2020-03-12 13:49:49 -0300

    Adding model compile function and bug fixes.

--- train/modeltrain.py
+++ train/modeltrain.py
@@ -1,8 +1,8 @@
 import pickle
 import tensorflow as tf
 import numpy as np
-import keras
-from keras.initializers import glorot_normal
+from tensorflow import keras
+from tensorflow.keras.initializers import glorot_normal
 from sklearn.model_selection import train_test_split
 from time import localtime, strftime
 from datetime import datetime
@@ -11,8 +11,9 @@ from train.trainingplot import TrainingPlot
 from train.confusionmatrix import ConfusionMatrix
 from sklearn import metrics
 
+
 class ModelTrain():
-    def __init__(self, model_name, batch_size, epochs, test_size=0.2, model_arq=None):
+    def __init__(self, model_name, batch_size, epochs, test_size=0.2, model_arq=None, lr=1.0, dlr=0.0):
         logger.debug(f"Initializating train of model {model_name}.")
         self.RUN = strftime("%Y-%m-%d_%H:%M:%S", localtime())
         self.batch_size = batch_size
@@ -20,16 +21,18 @@ class ModelTrain():
         self.set_model(model_arq)
         self.model_name = model_name
         self.logdir = f"{app_cfg['path']}/logs/{self.model_name + self.RUN}"
+        self.learning_rate = lr
+        self.decay_learning_rate = dlr
         logger.debug("Seting up Tensorflow configurations.")
         # Tensorflow Configurations
         gpu_options = tf.compat.v1.GPUOptions(
             per_process_gpu_memory_fraction=0.85)
         self.sess = tf.compat.v1.Session(
             config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
-        self.tb = keras.callbacks.TensorBoard(log_dir= self.logdir)
+        self.tb = keras.callbacks.TensorBoard(log_dir=self.logdir)
 
-        self.load_data(data_path =app_cfg['data_path'],test_size=test_size)
-        
+        self.load_data(data_path=app_cfg['data_path'], test_size=test_size)
+
     def run(self):
         self.model_train(batch_size=self.batch_size, epochs=self.epochs)
 
@@ -43,44 +46,69 @@ class ModelTrain():
         # Calculate the confusion matrix.
         cm = metrics.confusion_matrix(self.test_label, test_pred)
         # Save confusion matrix.
-        ConfusionMatrix(logdir=self.logdir).save_cm(cm, class_names=app_cfg['class_names'])
-
+        ConfusionMatrix(logdir=self.logdir).save_cm(
+            cm, class_names=app_cfg['class_names'])
 
     def load_data(self, data_path, test_size=0.15):
         logger.info("Loading data.")
         try:
             data = pickle.load(open(data_path, "rb"))
+            logger.debug(f"Loaded {len(data)} samples.")
         except Exception as err:
-            #TODO Exception
+            # TODO Exception
             logger.error(err)
-            exit
-        
-        logger.debug(f"Loaded {len(data)} samples.")
+            exit()
+
         X = []
         y = []
         for feature, label in data:
             X.append(feature)
             y.append(label)
-        
-        X = np.array(X).reshape(-1, app_cfg['img_size'], app_cfg['img_size'], 3)
+
+        X = np.array(
+            X).reshape(-1, app_cfg['img_size'], app_cfg['img_size'], 3)
         X = X/255.0
 
         y = keras.utils.to_categorical(y, num_classes=None)
 
         logger.info('Splitting dataset.')
-        #TODO find another way to split dataset, using too much memory.
-        self.train_data, self.test_data, self.train_label, self.test_label = train_test_split(X, y, test_size=test_size, random_state=1)
-        logger.info(f'Data load complete. (Train_data:{len(self.train_data)}; Test_data:{len(self.test_data)})')
+        # TODO find another way to split dataset, using too much memory.
+        self.train_data, self.test_data, self.train_label, self.test_label = train_test_split(
+            X, y, test_size=test_size, random_state=1)
+        logger.info(
+            f'Data load complete. (Train_data:{len(self.train_data)}; Test_data:{len(self.test_data)})')
 
     def set_model(self, model):
         logger.info("Setting model.")
         self.model = model
         logger.debug(f"Model set, summary: {self.model.summary()}")
 
+    def compile(self, loss_name='categorical_crossentropy', metrics=['accuracy'], optimizer_name='adam'):
+        logger.info(
+            f"Compiling model. (name_loss={loss_name}, metrics={metrics}, optimizer_name={optimizer_name})")
+
+        # Optimizer
+        if optimizer_name == 'adam':
+            optimizer = keras.optimizers.Adam(
+                lr=self.learning_rate, decay=self.decay_learning_rate)
+        elif optimizer_name == 'rmsprop':
+            optimizer = keras.optimizers.RMSprop(
+                lr=self.learning_rate, decay=self.decay_learning_rate)
+        elif optimizer_name == 'sgd':
+            optimizer = keras.optimizers.SGD(
+                lr=self.learning_rate, decay=self.decay_learning_rate, momentum=0.9, nesterov=None)
+        else:
+            raise RuntimeError(
+                'Optimizer should be one of Adam, RMSprop and SGD.')
+        self.model.compile(
+            loss=loss_name, optimizer=optimizer, metrics=metrics)
+
     def model_train(self, batch_size, epochs):
-        logger.info(f"Starting model training. [batch_size={batch_size}, epochs={epochs}]")
+        logger.info(
+            f"Starting model training. [batch_size={batch_size}, epochs={epochs}]")
         start_time = datetime.now()
-        history = self.model.fit(self.train_data, self.train_label, batch_size=batch_size, validation_data=(self.test_data, self.test_label), epochs=epochs, callbacks=[self.tb])
+        history = self.model.fit(self.train_data, self.train_label, batch_size=batch_size, validation_data=(
+            self.test_data, self.test_label), epochs=epochs, callbacks=[self.tb])
         end_time = datetime.now()
         logger.info(f"Model train ended. (Train time:{end_time - start_time})")
         logger.info("Saving model.")
@@ -89,7 +117,9 @@ class ModelTrain():
             logger.info(f"Model saved.")
         except Exception as err:
             logger.error(err)
-    
+
         logger.info("Plotting accuracy/loss graphics.")
         TrainingPlot(outputdir=self.logdir, history=history)
-        #self.log_confusion_matrix()
+
+        # TODO Fix Confusion Matrix
+        # self.log_confusion_matrix()
